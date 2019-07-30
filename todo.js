986d50a15f79d225c6e63b7c5cc682a0c40b3220
chance <ch4nc3l0@gmail.com>
2019-07-30 11:59:55 -0400

    Feat: add intital js

---
+++ todo.js
@@ -0,0 +1,12 @@
+// make todo array
+
+// make every todo an object with marked, checked off, date values, is working on
+
+// be able to add todos
+
+// be able to check off todos
+
+// be able to delete todos
+
+// be able to sort todos
+
