63a0559c578ab676739fdd0c215fa22b5d594935
chance <ch4nc3l0@gmail.com>
2019-07-30 12:14:33 -0400

    Feat: insert js data into dom

--- todo.js
+++ todo.js
@@ -1,6 +1,14 @@
 // make todo array
+let todo = [
+    {data: "Walk the dog",
+     isChecked: false,
+     timeStamp: "7/30/2019",
+     isWorkingOn:false
+    }
+];
 
 // make every todo an object with marked, checked off, date values, is working on
+// example generated with default values
 
 // be able to add todos
 
@@ -9,4 +17,11 @@
 // be able to delete todos
 
 // be able to sort todos
+
+// add todos to DOM
+    let todoItem = document.getElementById("todoItem");
+
+    todo.forEach(item=>{
+        todoItem.append(item.data)
+    })
 
