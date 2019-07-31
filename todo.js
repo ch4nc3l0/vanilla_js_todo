4c90a5ce549e9917c18a502d5aa732885875b7f0
chance <ch4nc3l0@gmail.com>
2019-07-31 18:24:19 -0400

    Feat: add basic checkmark functionality

--- todo.js
+++ todo.js
@@ -1,3 +1,5 @@
+let todoItem = document.getElementById("todoItem");
+
 // make todo array
 let todo = [
     {data: "Walk the dog",
@@ -13,13 +15,40 @@ let todo = [
 // be able to add todos
 
 // be able to check off todos
+    
 
 // be able to delete todos
 
 // be able to sort todos
 
+// add css with javascript
+
+function isChecked(array){
+    // on todo item click check if todo is checked
+    array.forEach(item=>{
+        // if checked use checked.svg
+        if (item.isChecked === true){
+            todoItem.className = "checked"
+            // better logic should be used to check and uncheck the todos
+            item.isChecked = false
+        }
+        // if not checked use unchecked.svg
+        else {
+            todoItem.className = "unchecked"
+            item.isChecked = true
+        }
+    })
+}
+
+// 1: on startup run function to check if item is checked
+isChecked(todo);
+
+
+
+
+
 // add todos to DOM
-    let todoItem = document.getElementById("todoItem");
+
 
     todo.forEach(item=>{
         todoItem.append(item.data)
